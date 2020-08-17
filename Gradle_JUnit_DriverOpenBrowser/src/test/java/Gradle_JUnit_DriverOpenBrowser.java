7400c8f9a199c14e1250e9bfad50afb9641153e7
MohammadRishaRaditya <risha.raditya@gmail.com>
2020-08-17 15:24:42 +0700

    JUnit for Maven and Gradle

---
+++ Gradle_JUnit_DriverOpenBrowser/src/test/java/Gradle_JUnit_DriverOpenBrowser.java
@@ -0,0 +1,22 @@
+/**
+ * Created by risha on 17/08/20
+ */
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Gradle_JUnit_DriverOpenBrowser {
+
+    @Test
+    public void startSite() {
+        String url = "https://www.facebook.com";
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+
+        String title = driver.getTitle();
+        System.out.println("The title of " + url + " is: " + title);
+
+        driver.quit();
+    }
+}
